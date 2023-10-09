7cbcd02d5f7099663a1c13e23b88c45c72235e1e
Salman Shaikh <coder88salman@gmail.com>
2023-10-09 18:20:14 +0530

    Init

---
+++ src/app/layout.js
@@ -0,0 +1,36 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "PES College of Engineering - Aurangabad",
+  image: "",
+  type: "website",
+  description: "This is the leaderboard for the GDSC PESCOE Jams.",
+  keywords: ["gccp", "jams", "pescoe", "gdscpescoe"],
+  robots: "index,follow",
+
+  "og:title": "GDSC PESCOE Leaderboard",
+  "og:image": "",
+  "og:description": "This is the leaderboard for the GDSC PU Jams.",
+  "twitter:card": "summary_large_image",
+  "twitter:title": "GDSC PU Leaderboard",
+  "twitter:description": "This is the leaderboard for the GDSC PU Jams.",
+  "twitter:image": "",
+  "twitter:site": "@gdsc_pescoe",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <link rel="icon" href="/favicon.ico" />
+      <head></head>
+      <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
