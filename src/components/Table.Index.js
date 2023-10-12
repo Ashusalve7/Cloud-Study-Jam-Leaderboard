004fdd4b04daf301f6cc9a779d15106e43410fec
Salman Shaikh <coder88salman@gmail.com>
2023-10-12 19:05:01 +0530

    October 12th - 6:58 pm                Thursday, 12 October 2023 (IST)                Time in Nagsenvan, Aurangabad, Maharashtra

--- src/components/Table.Index.js
+++ src/components/Table.Index.js
@@ -46,9 +46,9 @@ function TableIndex() {
       <div className="sec m-auto my-10 space-y-8 w-1/2 mob:w-full flex flex-col">
 
         <div className="message bg-yellow-100 text-yellow-700 p-5 rounded-lg shadow-lg shadow-yellow-300/30 text-center border border-yellow-300/30"><p className="text-center">-:Notification:-</p>
-          <p>Last Updated 7:37 pm
-            Wednesday, 11 October 2023 (IST)
-            Time in Aurangabad, Maharashtra)</p>
+          <p>Last Updated 6:58 pm
+            Thursday, 12 October 2023 (IST)
+            Time in Nagsenvan, Aurangabad, Maharashtra</p>
         </div>
 
         {/*<Speedometer*/}
