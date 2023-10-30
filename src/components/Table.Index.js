9c222cdbf8c5752de678c1df484fdfad527f923a
Salman Shaikh <coder88salman@gmail.com>
2023-10-30 12:22:01 +0530

    October 30th - 6:51 pm               Sunday, 29 October 2023 (IST)               Time in Parbhani, Maharashtra

--- src/components/Table.Index.js
+++ src/components/Table.Index.js
@@ -114,7 +114,7 @@ function TableIndex() {
 
         <div className="info flex mob:flex-col mob:justify-center mob:items-center mob:space-y-10 mob:p-5 justify-evenly space-x-3 mob:space-x-0">
           <div className="eligibleforswag w-fit mob:w-full h-20 p-5 space-x-5 rounded-lg flex flex-row justify-evenly mob:justify-between items-center bg-green-50 shadow-lg shadow-green-300/30 border border-green-200">
-            <p className="text-center mob:text-start text-sm text-green-400">No of Eligible <br /> Participants for Certification & Rewards</p>
+            <p className="text-center mob:text-start text-sm text-green-400">No of Eligible <br /> Participants for Certification</p>
             <p className="no text-2xl border-l-2 border-l-green-700 pl-3 text-green-800">{EligibleforSwags}</p>
           </div>
           <div className="eligibleforswag w-fit mob:w-full h-20 p-5 space-x-5 rounded-lg flex flex-row justify-evenly mob:justify-between items-center bg-blue-50 shadow-lg shadow-blue-300/30 border border-blue-200">
